b127ab1269b3d1218069fc221034bd967f323fce
Matthew <metju312@gmail.com>
2017-07-21 02:23:51 +0200

    added trunk methods

--- src/main/java/ModelOOP/Trunk.java
+++ src/main/java/ModelOOP/Trunk.java
@@ -1,4 +1,19 @@
 package ModelOOP;
 
 public class Trunk {
+
+    private int size;
+
+    public Trunk(int size) {
+        this.size = size;
+
+    }
+
+    public void grow() {
+        size++;
+    }
+
+    public void print() {
+        System.out.println("TrunkSize: " + size);
+    }
 }
