eb62715a0fa2d9b138d06431882c0f86ec965024
Matthew <metju312@gmail.com>
2017-07-21 02:25:12 +0200

    added leaf methods

--- src/main/java/ModelOOP/Leaf.java
+++ src/main/java/ModelOOP/Leaf.java
@@ -1,4 +1,22 @@
 package ModelOOP;
 
 public class Leaf {
+
+    private int size;
+
+    public Leaf(int size) {
+        this.size = size;
+    }
+
+    public void print() {
+        System.out.print(size);
+    }
+
+    public void grow() {
+        size++;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
