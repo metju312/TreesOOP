7b00ced4cfb404124a4febb733a917cf7d08e0c1
Matthew <metju312@gmail.com>
2017-07-21 02:24:53 +0200

    added abstract to Tree/ Tree methods / abstract Tree methods using by extended LeafTree and ConiferTree

--- src/main/java/ModelOOP/Tree.java
+++ src/main/java/ModelOOP/Tree.java
@@ -1,4 +1,63 @@
 package ModelOOP;
 
-public class Tree {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Tree {
+    public Trunk trunk;
+    public List<Branch> branches;
+    public LeafType leafType;
+    public int size;
+
+    public Tree(int size) {
+        this.size = size;
+        this.trunk = new Trunk(size);
+        this.branches = new ArrayList<Branch>();
+        addBranches();
+        addLeafs();
+    }
+
+    private void addBranches() {
+        for (int i = 0; i < size; i++) {
+            branches.add(new Branch(size));
+        }
+    }
+
+    public void grow(){
+        size++;
+        trunk.grow();
+        for (Branch branch : branches) {
+            branch.grow();
+        }
+        if(size%13 == 0){ //January grow
+            for (int i = 0; i < size/12 + 1; i++) {//every year one more branch to grow pool
+                branches.add(new Branch(1));
+            }
+        }
+
+        dropLeavesIfNeeded();
+        growLeaves();
+    }
+
+    public void addLeafs(){
+        setLeafType();
+    }
+
+    public void print() {
+        printTreeType();
+        trunk.print();
+        for (Branch branch : branches) {
+            branch.print();
+        }
+        System.out.println();
+    }
+
+    protected abstract void growLeaves();
+
+    protected abstract void printTreeType();
+
+    protected abstract void setLeafType();
+
+    protected abstract void dropLeavesIfNeeded();
+
 }
