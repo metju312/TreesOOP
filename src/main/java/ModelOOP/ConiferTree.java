49db0104e2d38694b218c5caeacf39683db98b70
Matthew <metju312@gmail.com>
2017-07-21 02:25:42 +0200

    added conifer tree physics

--- src/main/java/ModelOOP/ConiferTree.java
+++ src/main/java/ModelOOP/ConiferTree.java
@@ -1,4 +1,41 @@
 package ModelOOP;
 
-public class ConiferTree {
+public class ConiferTree extends Tree {
+
+    public ConiferTree(int size) {
+        super(size);
+    }
+
+    public void addLeafs() {
+        for (Branch branch : branches) {
+            branch.addLeafs();
+        }
+        dumpNeedlesIfOld();
+    }
+
+    protected void growLeaves() {
+        for (Branch branch : branches) {
+            branch.growLeaves(size);
+        }
+    }
+
+    private void dumpNeedlesIfOld() {
+        for (Branch branch : branches){
+            branch.dumpNeedlesIfOld();
+        }
+    }
+
+    protected void printTreeType() {
+        System.out.println("#################");
+        System.out.println("## ConiferTree ##");
+        System.out.println("#################");
+    }
+
+    protected void setLeafType() {
+        leafType = LeafType.NEEDLE;
+    }
+
+    protected void dropLeavesIfNeeded() {
+        dumpNeedlesIfOld();
+    }
 }
