298945461ab1abbd064239d9b51efd55d7dd2473
Matthew <metju312@gmail.com>
2017-07-21 02:22:17 +0200

    added two scenario of trees grow

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+import ModelOOP.ConiferTree;
+import ModelOOP.LeafyTree;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("d");
+        scenario1(2,26);
+        //scenario2(2,26);
+    }
+
+    private static void scenario1(int treeSize, int vegetationIterations) {
+        LeafyTree leafyTree = new LeafyTree(treeSize);
+        leafyTree.print();
+        for (int i = 0; i < vegetationIterations; i++) {
+            leafyTree.grow();
+            leafyTree.print();
+        }
+    }
+
+    private static void scenario2(int treeSize, int vegetationIterations) {
+        ConiferTree coniferTree = new ConiferTree(treeSize);
+        coniferTree.print();
+        for (int i = 0; i < vegetationIterations; i++) {
+            coniferTree.grow();
+            coniferTree.print();
+        }
     }
 }
